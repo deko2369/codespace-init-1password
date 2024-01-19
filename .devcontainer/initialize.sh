8f3b0fe63ebfd11ce6504eae9680b0dd89c4c060
deko2369 <deko2369@gmail.com>
2024-01-19 11:39:25 +0000

    fixed 1password installation command

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 set -e
 
-cd `mktemp -d`
+OP_TEMP_DIR=`mktemp -d`
+
+cd $OP_TEMP_DIR
 curl -sSO https://downloads.1password.com/linux/tar/stable/x86_64/1password-latest.tar.gz
 tar -xf 1password-latest.tar.gz
 mkdir -p /opt/1Password
-mv 1password-*/* /opt/1Password/
+cp -r ./1password-*/* /opt/1Password
 /opt/1Password/after-install.sh
-
 cd -
+
+rm -rf $OP_TEMP_DIR
